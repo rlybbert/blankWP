b8e793c381104d60577070d16c4d851277a5b8f1
Ben Buie <mail@benbuie.com>
2014-09-23 16:42:26 -0600

    mistake in footer file

--- wp-content/themes/blankWP/footer.php
+++ wp-content/themes/blankWP/footer.php
@@ -1,10 +1,9 @@
 <?php
 
 ?>
-		</div><!-- end of body div -->
+		</div>
 		<div class="footer clearfix">
-			I'm your footer file (footer.php) and here is your footer menu:
-     	 	<?php wp_nav_menu( array('theme_location' => 'footer-menu') ); ?>
+     		<?php wp_nav_menu( array('theme_location' => 'footer-menu') ); ?>
 			<div class="source-org vcard copyright" role="contentinfo">&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?></div>
 		</div>
 
